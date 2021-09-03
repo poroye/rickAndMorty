8e974e5886454902346c3af319920aa1fd8e9c69
tanat <31177223+poroye@users.noreply.github.com>
2021-09-03 03:18:14 -0700

    update detail
    just start to make detail view but have to move to another place =_=

---
+++ rickAndMorty/charDetail.swift
@@ -0,0 +1,25 @@
+//
+//  charDetail.swift
+//  rickAndMorty
+//
+//  Created by ธนัท แสงเพิ่ม on 3/9/2564 BE.
+//
+
+import SwiftUI
+
+struct charDetail: View {
+    
+    let charac:result
+    
+    var body: some View {
+        VStack{
+            Text(charac.name)
+        }
+    }
+}
+//
+//struct charDetail_Previews: PreviewProvider {
+//    static var previews: some View {
+//        charDetail()
+//    }
+//}
