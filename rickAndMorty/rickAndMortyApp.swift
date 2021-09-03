98c173112d6095af60483252695f412db3138fa1
pro tanut <60010427@kmitl.ac.th>
2021-09-03 01:17:10 -0700

    Initial Commit

---
+++ rickAndMorty/rickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickAndMortyApp.swift
+//  rickAndMorty
+//
+//  Created by ธนัท แสงเพิ่ม on 3/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct rickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
